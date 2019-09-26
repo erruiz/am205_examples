77cc0fce1ff76c9e3abb527e43158dd3882a2676
erruiz <eruiz@g.harvard.edu>
2019-09-25 23:53:11 -0400

    Updated `timing_test.py` for Python3
    The `time.clock()` routine was replaced with `time.process_time()` and
    parentheses were added to the `print` statements.

--- 2_num_lin_alg/timing_test.py
+++ 2_num_lin_alg/timing_test.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
-from time import clock,time
+from time import process_time, time
 import numpy as np
 import scipy.linalg
 
 # Get the current time() (a measure of "wall clock time", the physical passage of
 # time as you would observe by looking at a wall clock)
-t0=time()
+t0 = time()
 
-# Get the current clock() (a measure of the time that this program has actually
+# Get the current process_time() (a measure of the time that this program has actually
 # spent on the processor)
-c0=clock()
+c0 = process_time()
 
 # Carry out a large, arbitrary calculation
-k=0
+k = 0
 for i in range(1000):
 	for j in range(1000):
-		k+=i+j
+		k += i+j
 
-# Print the elapsed time, using both the time() routine and the clock() routine
-ttotal=time()-t0
-ctotal=clock()-c0
-print "time routine:",ttotal
-print "clock routine:",ctotal
-print "clock/time ratio:",ctotal/ttotal
+# Print the elapsed time, using both the time() routine and the process_time() routine
+ttotal = time()-t0
+ctotal = process_time()-c0
+print("time routine:", ttotal)
+print("clock routine:", ctotal)
+print("clock/time ratio:", ctotal/ttotal)
